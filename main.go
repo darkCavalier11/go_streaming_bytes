5a423cfff46a4b036c2ef7e4a35fce9207a21887
darCavalier11 <sumit.pradhan65@gmail.com>
2023-02-23 22:03:20 +0530

    added go ffmpeg

--- main.go
+++ main.go
@@ -1,17 +1,34 @@
 package main
 
 import (
+	"io"
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/kkdai/youtube/v2"
 )
 
 func main() {
 	v := make(chan int)
 	_ = v
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		values := r.URL.Query()
-		videoId := values.Get("videoId")
-		_ = videoId
-		w.Write([]byte("Hello World"))
+		client := youtube.Client{}
+		
+		video, err := client.GetVideo("Obim8BYGnOE")
+
+
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		audioFormat := video.Formats.FindByItag(140)
+
+		stream, _, err := client.GetStream(video, audioFormat)
+		file, _ := os.Create("music.m4a")
+
+		io.Copy(file, stream)
 	})
 
 	http.ListenAndServe(":8080", nil)
