024a5feff9cd6f7ea900382dee77d9071fe677b0
darCavalier11 <sumit.pradhan65@gmail.com>
2023-02-24 21:43:52 +0530

    system config ffmpeg

--- main.go
+++ main.go
@@ -1,35 +1,37 @@
 package main
 
 import (
-	"io"
-	"log"
-	"net/http"
-	"os"
+	"fmt"
 
-	"github.com/kkdai/youtube/v2"
+	ffmpeg_go "github.com/u2takey/ffmpeg-go"
 )
 
 func main() {
 	v := make(chan int)
 	_ = v
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		client := youtube.Client{}
-		
-		video, err := client.GetVideo("Obim8BYGnOE")
+	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// client := youtube.Client{}
 
+	// video, err := client.GetVideo("Obim8BYGnOE")
 
-		if err != nil {
-			log.Println(err)
-			return
-		}
+	// if err != nil {
+	// 	log.Println(err)
+	// 	return
+	// }
 
-		audioFormat := video.Formats.FindByItag(140)
+	// audioFormat := video.Formats.FindByItag(140)
 
-		stream, _, err := client.GetStream(video, audioFormat)
-		file, _ := os.Create("music.m4a")
+	// stream, _, err := client.GetStream(video, audioFormat)
+	// file, _ := os.Create("music.m4a")
+	ffStream := ffmpeg_go.Input("music.m4a")
+	// io.Copy(file, stream)
+	// fmt.Print("Here")
+	err := ffStream.Output("music.mp3", ffmpeg_go.KwArgs{
+		"c:a": "libmp3lame",
+		"q:a": "8",
+	}).OverWriteOutput().ErrorToStdOut().Run()
+	fmt.Println(err)
+	// })
 
-		io.Copy(file, stream)
-	})
-
-	http.ListenAndServe(":8080", nil)
+	// http.ListenAndServe(":8080", nil)
 }
